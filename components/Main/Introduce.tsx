51e9c8120b1fe23bcee26bc2384db5b987f0943f
sangil94s <bsc7417@gmail.com>
2025-04-01 22:01:02 +0900

    [Modify] 일부 파일 수정

--- components/Main/Introduce.tsx
+++ components/Main/Introduce.tsx
@@ -3,8 +3,12 @@
 export default function Introduce() {
     return (
        
-            <div className="bg-gray-100 w-full h-full lg:w-[1920px] lg:h-[270px]">
-                <h1 className="py-10 text-red-600 text-center">첫 소개 부분</h1>
+            <div className="flex flex-row justify-center items-center mt-2 bg-gray-100 w-full h-full lg:w-[1920px] lg:h-[270px]">
+                <section className="border border-slate-300 rounded-full w-[200px] h-[200px]">
+                    <p>사진</p>
+                </section>
+                <h1 className="p-10 text-red-600 text-center">첫 소개 부분</h1>
+
             </div>
        
     )
