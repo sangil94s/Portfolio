840af6d9d596b51a248a0f74d9f8456d24149c6a
한상일 <hansang-il@hansang-il-ui-MacBookAir.local>
2025-04-02 13:58:29 +0900

    [Add] Skill 이미지 파일 추가

--- components/Main/Skill.tsx
+++ components/Main/Skill.tsx
@@ -1,9 +1,11 @@
+import Image from 'next/image';
+
 // 스킬을 소개하는 부분
 
 export default function Skill() {
   return (
     <>
-      <div className="bg-gray-400 w-full h-full lg:w-[1920px] lg:h-[270px]">
+      <div className="bg-gray-400 w-full h-max lg:w-[1920px]">
         <h1 className="py-4 text-red-600 text-2xl text-center font-bold">Skill</h1>
 
         <section className="w-6/12 border border-slate-300 rounded-md m-auto py-4">
@@ -12,6 +14,7 @@ export default function Skill() {
 
         <section className="w-6/12 border border-slate-300 rounded-md m-auto py-4 my-2">
           <p>백엔드 파트</p>
+          <Image src={'/skill/express.png'} width={100} height={100} alt="Express-logo" className="rounded-full" />
         </section>
 
         <section className="w-6/12 border border-slate-300 rounded-md m-auto py-4">
