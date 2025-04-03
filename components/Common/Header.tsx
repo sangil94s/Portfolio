7c7d37b53494952904b62f9cd5759b357c463993
한상일 <hansang-il@hansang-il-ui-MacBookAir.local>
2025-04-03 15:08:49 +0900

    [Modify] Skill.tsx - 이미지에 마우스 올리면 스킬 명 나오도록 수정

--- components/Common/Header.tsx
+++ components/Common/Header.tsx
@@ -6,9 +6,9 @@ export default function Header() {
       <h1 className="text-xl text-white font-bold p-2">헤더</h1>
 
       <section className="flex gap-2 p-2">
-        <h1 className="text-white font-bold">Menu1</h1>
-        <h1 className="text-white font-bold">Menu2</h1>
-        <h1 className="text-white font-bold">Menu3</h1>
+        <h1 className="text-white font-bold">기술</h1>
+        <h1 className="text-white font-bold">경력</h1>
+        <h1 className="text-white font-bold">연락처</h1>
       </section>
     </header>
   );
--- components/Main/Contact.tsx
+++ components/Main/Contact.tsx
@@ -9,16 +9,20 @@ export default function Contact() {
     <>
       <div className="bg-black w-full h-full lg:w-[1920px] lg:h-[270px]">
         <h1 className="py-4 text-white text-2xl text-center font-bold">연락처</h1>
-        <p className="py-2 text-white text-base text-center">봐주셔서 감사드리며, 아래를 통해 저를 더 확인할 수 있습니다.</p>
+        <p className="py-2 text-white text-base text-center">
+          봐주셔서 감사드리며, 아래를 통해 저를 더 확인할 수 있습니다.
+        </p>
 
         <section className="flex flex-row justify-center items-center gap-6 my-4 text-white text-xl lg:text-4xl">
           <Link href="https://github.com/sangil94s">
             <AiFillGithub />
           </Link>
           <Link href="mailto:bsc5732@naver.com">
-          <AiFillMail />
+            <AiFillMail />
           </Link>
-          <RiNotionFill />
+          <Link href="https://precious-hyssop-b3b.notion.site/2-1c850042c17080c8a979c087ba43d1f9?pvs=4">
+            <RiNotionFill />
+          </Link>
         </section>
       </div>
     </>
