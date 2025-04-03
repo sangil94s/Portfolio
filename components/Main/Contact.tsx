d4452b80193b99c6113afeccbc013c6478299920
한상일 <hansang-il@MacBookAir.localdomain>
2025-04-03 14:02:17 +0900

    [Modify] Contract 수정

--- components/Main/Contact.tsx
+++ components/Main/Contact.tsx
@@ -8,14 +8,16 @@ export default function Contact() {
   return (
     <>
       <div className="bg-black w-full h-full lg:w-[1920px] lg:h-[270px]">
-        <h1 className="py-4 text-white text-2xl text-center font-bold">연락처 소개 부분</h1>
-        <p className="py-2 text-white text-xl text-center">대충 연락을 기다리고 있다는 포장글</p>
+        <h1 className="py-4 text-white text-2xl text-center font-bold">연락처</h1>
+        <p className="py-2 text-white text-base text-center">봐주셔서 감사드리며, 아래를 통해 저를 더 확인할 수 있습니다.</p>
 
         <section className="flex flex-row justify-center items-center gap-6 my-4 text-white text-xl lg:text-4xl">
           <Link href="https://github.com/sangil94s">
             <AiFillGithub />
           </Link>
+          <Link href="mailto:bsc5732@naver.com">
           <AiFillMail />
+          </Link>
           <RiNotionFill />
         </section>
       </div>
