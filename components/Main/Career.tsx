79ac2d4fad11e4f8fc14bf585cd3454642766c5b
sangil94s <bsc7417@gmail.com>
2025-04-04 20:51:07 +0900

    [Modify] Introduce 등 수정

--- components/Main/Career.tsx
+++ components/Main/Career.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { RiNotionFill } from 'react-icons/ri';
 
 // 경력 사항 소개 부분
 
@@ -11,10 +12,28 @@ export default function Career() {
           <h1 className="p-4 text-white text-2xl text-center font-bold">Career</h1>
           <hr className="flex-grow border-gray-300" />
         </section>
-        <div className="flex flex-col justify-center items-center gap-6 lg:gap-22 lg:flex-row">
-          <Image src={'/Logos.jpg'} width={100} height={100} alt="회사 로고" className="rounded-full" />
-          <ul className="list-disc text-white self-center">
-            <li>1</li>
+        <div className="flex flex-col justify-center items-center gap-6 lg:gap-10 lg:flex-row">
+          <section className="py-1">
+            <Image src={'/Logos.jpg'} width={140} height={100} alt="회사 로고" className="rounded-full" />
+            <p className="text-white font-bold">근무 : 2022.12 - 2025.03 </p>
+          </section>
+          <ul className="list-disc text-white">
+            <li className="p-2 font-bold">입사 2달 만에 EJS → React 전환을 제안하고 실행.</li>
+            <li className="p-2 font-bold">API 문서조차 없던 환경에 Notion 기반 문서화를 도입, 2개 프로젝트에 적용</li>
+            <li className="p-2 font-bold">
+              웹에 무관심한 제조업 기업에서 프론트·백엔드 전체를 단독 개발하며 실무 전반을 경험
+            </li>
+            <section className="grid grid-cols-3 gap-2 text-white text-xl justify-items-center py-4">
+              <p>
+                <RiNotionFill />{' '}
+              </p>
+              <p>
+                <RiNotionFill />
+              </p>
+              <p>
+                <RiNotionFill />
+              </p>
+            </section>
           </ul>
         </div>
       </div>
--- components/Main/Introduce.tsx
+++ components/Main/Introduce.tsx
@@ -1,14 +1,15 @@
-import Image from 'next/image';
-
 // 아마도 첫 소개에 배치되는 부분
 
 export default function Introduce() {
   return (
-    <div className="flex flex-col justify-center items-center mt-2 w-full h-full lg:flex-row lg:w-[1920px] lg:h-[270px]">
-      <section className="border border-slate-300 rounded-full w-[200px] h-[200px]">
-        <Image src={'/Profile.png'} width={200} height={200} alt="프로필 이미지" />
-      </section>
-      <h1 className="p-10 text-white text-center">첫 소개 부분</h1>
+    <div className="flex flex-col justify-center items-center mt-2 w-full h-full lg:w-[1920px] lg:h-[270px]">
+      <h1 className="p-2 text-white text-4xl text-center font-bold">주도적인 2년차 개발자 한상일입니다</h1>
+      <p className="pb-2 text-white text-xl text-center">
+        React로 프론트엔드 개발을 주로 하며, Express로 백엔드 개발도 가능합니다.
+      </p>
+      <p className="pb-2 text-white text-xl text-center">
+        담당 업무가 아니더라도 필요하다면 먼저 나서서 제안하거나 진행하는 주도적인 사람입니다.
+      </p>
     </div>
   );
 }
