1a9996ecd775b16d7fba65e1fb3fe5a2501fc3a6
sangil94s <bsc7417@gmail.com>
2025-04-06 14:47:50 +0900

    [Modify] Footer 수정

--- components/Common/Footer.tsx
+++ components/Common/Footer.tsx
@@ -3,8 +3,8 @@
 export default function Footer() {
   return (
     <footer className="w-full h-10">
-      <p className="text-center text-xs text-slate-300 font-bold py-2">
-        본 프로젝트는 Next.js 15 + TailwindCSS 4 버전으로 진행.
+      <p className="text-center text-xs text-red-600 font-bold py-2">
+        본 프로젝트는 FHD 해상도 기준으로 진행되었습니다
       </p>
     </footer>
   );
